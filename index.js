fef74602cb6496f6dbed2c24831636f5c377ad8c
Abhay <abhay@speechgears.com>
2025-11-01 19:26:52 +0530

    cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = 3001;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://osit-assignment-fd.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
